506277539c5a72de1776d008e399036291299edd
Mizu <5565979+l4desu-mizu@users.noreply.github.com>
2023-06-19 22:27:02 +0200

    add some code that should compile

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
-#[no_std]
-#[no_main]
-fn main() {
+#![no_std]
+#![no_main]
+extern crate panic_halt;
+
+use arduino_hal::Peripherals;
+
+#[arduino_hal::entry]
+fn main() -> ! {
+    let dp = Peripherals::take().unwrap();
+    dp.PORTD.ddrd.modify(|r,w|{
+        unsafe{w.bits(r.bits() | 1<<7)}
+    });
+    loop{}
 }
