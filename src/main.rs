f33eeb768916014fc49728f797d75f04a8f133be
Mizu <5565979+l4desu-mizu@users.noreply.github.com>
2023-06-20 01:42:11 +0200

    make it build

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 #![no_std]
 #![no_main]
-extern crate panic_halt;
+
+use panic_halt as _;
 
 use arduino_hal::Peripherals;
 
 #[arduino_hal::entry]
 fn main() -> ! {
     let dp = Peripherals::take().unwrap();
-    dp.PORTD.ddrd.modify(|r,w|{
-        unsafe{w.bits(r.bits() | 1<<7)}
-    });
-    loop{}
+    dp.PORTD
+        .ddrd
+        .modify(|r, w| unsafe { w.bits(r.bits() | 1 << 7) });
+    loop {}
 }
