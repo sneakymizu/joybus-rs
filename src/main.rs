02a7328f20f087f731c8f9dcb0a4a77c71224e1b
Mizu <5565979+l4desu-mizu@users.noreply.github.com>
2023-06-23 01:06:04 +0200

    enable read write from one pin

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,36 @@
 #![no_std]
 #![no_main]
-#![feature(abi_avr_interrupt)]
 
-use avr_device::interrupt::Mutex;
+use core::mem::size_of;
 use arduino_hal::prelude::*;
 use arduino_hal::port::mode::{Input, Output, PullUp};
 use arduino_hal::port::{Pin, PinOps};
 use panic_halt as _;
-use ufmt::{uwriteln};
+use ufmt::uwriteln;
 
-
-fn send_poll_signal<PIN: PinOps>(pin: &mut Pin<Output, PIN>){
-    pin.set_high();
+struct SwitchablePin<PIN: PinOps>{
+    read_pin: Option<Pin<Input<PullUp>, PIN>>,
+    write_pin: Option<Pin<Output, PIN>>
 }
-fn read_poll_data<PIN: PinOps>(pin: &Pin<Input<PullUp>, PIN>){
-    pin.is_high();
-    pin.is_low();
+impl<PIN: PinOps> SwitchablePin<PIN>{
+    fn from_output(pin: Pin<Output, PIN>) -> Self{
+        SwitchablePin {
+            read_pin: None,
+            write_pin: Some(pin),
+        }
+    }
+    fn as_output(&mut self)->Option<&mut Pin<Output, PIN>>{
+        if self.read_pin.is_some(){
+            self.write_pin = Some(self.read_pin.take().unwrap().into_output())
+        }
+        self.write_pin.as_mut()
+    }
+    fn as_input(&mut self)->Option<&mut Pin<Input<PullUp>, PIN>>{
+        if self.write_pin.is_some(){
+            self.read_pin = Some(self.write_pin.take().unwrap().into_pull_up_input());
+        }
+        self.read_pin.as_mut()
+    }
 }
 
 #[arduino_hal::entry]
@@ -25,34 +40,47 @@ fn main() -> ! {
     let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
     // Digital pin 13 is also connected to an onboard LED marked "L"
     let mut led_pin = pins.d13.into_output();
-    let mut data_pin = pins.a0.into_output();
+    let mut data_pin = SwitchablePin::from_output(pins.a0.into_output());
     led_pin.set_high();
     loop {
-        data_pin.send_bits(7);
-        //data_pin.take().into_output().send_bits(8);
-        //data_pin.as_ptr().into_pull_up_input().read_bits();
+        match data_pin.as_output(){
+            Some(pin)=>pin.send_bits(3),
+            None=>()
+        };
+        let read_bits = match data_pin.as_input(){
+            Some(pin)=>pin.read_bits(),
+            None=>0
+        };
         led_pin.toggle();
         arduino_hal::delay_ms(200);
         led_pin.toggle();
         arduino_hal::delay_ms(200);
-        uwriteln!(&mut serial, "Should read {}", 1).void_unwrap();
+        uwriteln!(&mut serial, "Should read {}", read_bits).void_unwrap();
     }
 }
 
 trait SendN64Bits{
-    fn send_bits(&mut self, data: u8);
+    fn send_bits(&mut self, data: usize);
 }
 trait RecvN64Bits{
-    fn read_bits(&self, )->u8;
+    fn read_bits(&self)->u32;
 }
 
 impl<PIN: PinOps> SendN64Bits for Pin<Output, PIN>{
-    fn send_bits(&mut self, data: u8) {
-        todo!()
+    fn send_bits(&mut self, data: usize) {
+        for count in 0..=size_of::<usize>()*8{
+            if data & 1<<count != 0{
+                self.set_high();
+            }else{
+                self.set_low();
+            }
+        }
     }
 }
 impl<PIN: PinOps> RecvN64Bits for Pin<Input<PullUp>, PIN>{
-    fn read_bits(&self, ) -> u8 {
-        todo!()
+    fn read_bits(&self) -> u32 {
+        self.is_high();
+        self.is_low();
+        0
     }
 }
