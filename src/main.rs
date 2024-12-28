f59d3ec2aa76da85539b80950342906710a6192e
Mizu <5565979+l4desu-mizu@users.noreply.github.com>
2024-12-28 22:15:05 +0100

    decide against interrupt as all higher level languages will add guardrails that will exceed the 16 ops we have per µs

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,8 @@ mod same_pin_io;
 const READ_COMMAND: u16 = 0b11;
 const READ_COMMAND_LENGTH: u8 = 9;
 const STATE_RESPONSE_LENGTH: u8 = 32;
-/*
+
+/* // interrupts have too much overhead with ops, jumps and guardrails added by the compiler, prolly better to have read/write in code directly
 struct InterruptState {
     peripherals: Mutex<*mut u8>,
 }
@@ -34,40 +35,40 @@ fn TIMER0_COMPA(){
     unsafe {
         let cs = CriticalSection::new();
         let reg_val = state.peripherals.borrow(cs).read();
-        state.peripherals.borrow(cs).write(reg_val^2);
         core::sync::atomic::compiler_fence(core::sync::atomic::Ordering::SeqCst);
-    };
+        state.peripherals.borrow(cs).write(reg_val^4);
+    }
 }
-fn do_interrupts(){
+fn setup_interrupt_data(dp: &Peripherals){
     dp.TC0.timsk0.write(|w|w.ocie0a().set_bit());
     let ocra0 = Mutex::new(dp.TC0.ocr0a.as_ptr());
     unsafe {
         INTERRUPT_STATE = mem::MaybeUninit::new(InterruptState{peripherals: ocra0});
         core::sync::atomic::compiler_fence(core::sync::atomic::Ordering::SeqCst);
     }
-    unsafe {avr_device::interrupt::enable();}
 }
-*/
+ */
 
 #[arduino_hal::entry]
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
-    // Digital pin 13 is also connected to an onboard LED marked "L"
-    let mut led_pin = pins.d13.into_output();
-    pins.d6.into_output_high();
-    dp.TC0.tccr0a.write(|w|w.wgm0().pwm_phase().com0a().match_toggle());
-    dp.TC0.tccr0b.write(|w|w.cs0().prescale_8().wgm02().set_bit()); // set wgm02 for fastpwm with top = OCRA
-    dp.TC0.ocr0a.write(|w|w.bits(1)); // set OCRA = 16 for 1µs tick or prescale 8 for nums in µs
-    
-    led_pin.set_high();
 
+    // Digital pin 13 is also connected to an onboard LED marked "L"
+    let mut led_pin = pins.d13.into_output_high();
+    
+    // D6 is OCA0 which will be toggled by timer0
+    pins.d6.into_output_high();
+    arduino_hal::delay_ms(1000);
+    dp.TC0.tccr0a.write(|w|w.wgm0().pwm_fast().com0a().match_toggle());
+    dp.TC0.tccr0b.write(|w|w.cs0().prescale_8().wgm02().set_bit()); // set wgm02 for OCRA = top
+    dp.TC0.ocr0a.write(|w|w.bits(5)); // set OCRA = 15 for 1µs tick or prescale 8 to have each digit be 0.5 µs (starting at 0=0.5µ 1=1µ..)
+    
 
     //let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
-
     //uwriteln!(serial, "Lets go\r").unwrap();
+
     led_pin.set_low();
-    
     loop {
         if dp.TC0.tifr0.read().ocf0a().bit_is_set(){
             dp.TC0.ocr0a.modify(|r,w|{
