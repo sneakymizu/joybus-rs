6202ba51dda9e98a3e5c3dda15ca241e8018bda8
Mizu <5565979+l4desu-mizu@users.noreply.github.com>
2025-01-04 22:53:07 +0100

    use original n64 polling assembly specifically for atmega328p
    * interrupts are too slow so it needs to just set and read the pins directly in the given time

--- src/main.rs
+++ src/main.rs
@@ -1,126 +1,92 @@
 #![no_std]
 #![no_main]
 #![feature(asm_experimental_arch)]
+#![feature(asm_const)]
 
-use crate::same_pin_io::SwitchablePin;
-use arduino_hal::port::{
-    mode::{Input, Output, PullUp},
-    Pin, PinOps,
-};
-use core::arch::asm;
 use panic_halt as _;
-use ufmt::uwriteln;
+use ufmt::{derive::uDebug, uDebug, uDisplay, uwriteln};
 
-mod same_pin_io;
-
-const READ_COMMAND: u16 = 0b11;
-const READ_COMMAND_LENGTH: u8 = 9;
-const STATE_RESPONSE_LENGTH: u8 = 32;
-
-fn wait_1us() {
-    // these are magic... changing any instruction will result in unexpected faster execution
-    unsafe {
-        asm!(
-            "ldi {RTMP}, 3",
-            "1:",
-            "dec {RTMP}",
-            "brne 1b",
-            RTMP = in(reg) 4u8,
-        );
+pub struct N64ControllerState(u32);
+#[derive(uDebug)]
+pub enum ReadError{
+    Failed
+}
+impl uDisplay for ReadError{
+    fn fmt<W>(&self, fmt: &mut ufmt::Formatter<'_, W>) -> Result<(), W::Error>
+    where
+        W: ufmt::uWrite + ?Sized {
+        uDebug::fmt(&self, fmt)
     }
 }
-fn wait_2us() {
-    // this does not need to be as precise. Reading in the middle of a bit should be
-    // sufficient
-    wait_1us();
-    wait_1us();
-}
-fn wait_3us() {
-    // these are magic... changing any instruction will result in unexpected faster execution
-    unsafe {
-        asm!(
-            "ldi {RTMP}, 13",
-            "1:",
-            "dec {RTMP}",
-            "brne 1b",
-            RTMP = in(reg) 15u8,
-        )
-    }
+pub trait PollN64{
+    fn read(&self)->Result<N64ControllerState, ReadError>;
 }
 
-struct N64Communicator<PIN: PinOps>(SwitchablePin<PIN>);
-impl<PIN: PinOps> N64Communicator<PIN> {
-    fn new(mut pin: SwitchablePin<PIN>) -> Self {
-        if let Some(pin) = pin.as_output() {
-            pin.set_high();
-        }
-        Self(pin)
-    }
-    fn read(&mut self) -> Result<u32, u8> {
-        let mut output_pin = self.0.as_output().ok_or(2u8)?;
-        N64PollsignalSender::new(&mut output_pin).send()?;
-        let input_pin = self.0.as_input().ok_or(2u8)?;
-        Ok(N64ResponseReceiver::new(&input_pin).read()?)
-    }
-}
+#[cfg(atmega328p)]
+mod atmega328p_read{
+    use core::arch::asm;
+    use super::{PollN64, N64ControllerState, ReadError};
 
-type OutputPin<PIN> = Pin<Output, PIN>;
-struct N64PollsignalSender<'a, PIN> {
-    pin: &'a mut OutputPin<PIN>,
-}
-impl<'a, PIN: PinOps> N64PollsignalSender<'a, PIN> {
-    pub fn new(pin: &'a mut OutputPin<PIN>) -> Self {
-        N64PollsignalSender { pin }
-    }
-    pub fn send(&mut self) -> Result<(), u8> {
-        for i in (0..READ_COMMAND_LENGTH).rev() {
-            if 1 << i & READ_COMMAND > 0 {
-                self.send_1()?;
-            } else {
-                self.send_0()?;
+    pub struct Reader;
+    impl PollN64 for Reader{
+        fn read(&self)->Result<N64ControllerState, ReadError>{
+            unsafe{
+                let zeros:u8 = 7;
+                let ones:u8 = 2;
+                asm!{
+                    //poll
+                    "sbi {port}, {pin}", // PORTB Pin "6"
+                    "sbi {ddr}, {pin}", // DDRB Pin "6"
+                    "0:",
+                        "cbi {port}, {pin}",
+                        "ldi {rtmp}, 15",
+                        "10:",
+                            "dec {rtmp}",
+                            "brne 10b",
+                        "nop",
+                        "sbi {port}, {pin}",
+                        "ldi {rtmp}, 3",
+                        "11:",
+                            "dec {rtmp}",
+                            "brne 11b",
+                        "nop",
+                        "nop",
+                        "dec {zeros}",
+                        "brne 0b",
+                    "1:",
+                        "cbi {port}, {pin}",
+                        "ldi {rtmp}, 4",
+                        "10:",
+                            "dec {rtmp}",
+                            "brne 10b",
+                        "nop",
+                        "nop",
+                        "sbi {port}, {pin}",
+                        "ldi {rtmp}, 14",
+                        "11:",
+                            "dec {rtmp}",
+                            "brne 11b",
+                        "nop",
+                        "dec {ones}",
+                        "brne 1b",
+                    "nop",
+                    //switch to read
+                    "sbi {port}, {pin}",
+                    "cbi {ddr}, {pin}",
+                    zeros=in(reg) zeros,
+                    ones=in(reg) ones,
+                    rtmp=out(reg)_,
+                    port=const 0x0b,
+                    ddr=const 0x0a,
+                    pin=const 0x06
+                }
             }
-        }
-        Ok(())
-    }
-    fn send_0(&mut self) -> Result<(), u8> {
-        self.pin.set_low();
-        wait_3us();
-        self.pin.set_high();
-        wait_1us();
-        Ok(())
-    }
-    fn send_1(&mut self) -> Result<(), u8> {
-        self.pin.set_low();
-        wait_1us();
-        self.pin.set_high();
-        wait_3us();
-        Ok(())
-    }
-}
-
-type InputPin<PIN> = Pin<Input<PullUp>, PIN>;
-struct N64ResponseReceiver<'a, PIN> {
-    pin: &'a InputPin<PIN>,
-}
-impl<'a, PIN: PinOps> N64ResponseReceiver<'a, PIN> {
-    pub fn new(pin: &'a InputPin<PIN>) -> Self {
-        N64ResponseReceiver { pin }
-    }
-    pub fn read(&mut self) -> Result<u32, u8> {
-        let mut res: u32 = 0;
-        for bit in 0..STATE_RESPONSE_LENGTH {
-            wait_2us();
-            res |= (self.pin.is_high() as u32) << bit;
-            wait_2us();
-        }
-        wait_2us();
-        if self.pin.is_low() {
-            Err(3u8)
-        } else {
-            Ok(res)
+            Ok(N64ControllerState(1))
         }
     }
 }
+#[cfg(atmega328p)]
+pub use atmega328p_read::Reader;
 
 #[arduino_hal::entry]
 fn main() -> ! {
@@ -131,16 +97,16 @@ fn main() -> ! {
     led_pin.set_high();
 
     let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
-    let pin = SwitchablePin::from_output(pins.d6.into_output());
-    let mut n64 = N64Communicator::new(pin);
+    let _reader_pin = pins.d6.into_output_high();
+    let n64 = Reader;
 
     uwriteln!(serial, "Lets go\r").unwrap();
     led_pin.set_low();
     loop {
         let _write = match n64.read() {
-            Ok(num) => uwriteln!(serial, "Have read {}\r", num),
+            Ok(num) => uwriteln!(serial, "Have read {}\r", num.0),
             Err(e) => uwriteln!(serial, "Error occured {}\r", e),
         };
-        arduino_hal::delay_ms(1);
+        arduino_hal::delay_ms(1000);
     }
 }
