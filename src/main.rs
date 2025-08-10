2c0e10f313e7083302e57bf583949fe8c44d13f4
Mizu <5565979+l4desu-mizu@users.noreply.github.com>
2025-08-10 17:09:20 +0200

    remove embassy from root project

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,13 @@
 #![no_std]
 #![no_main]
 
-use esp_hal::{gpio, main, peripherals::Peripherals};
-use ufmt::uwriteln;
+use embassy_executor::{main, Spawner};
 
 const READ_COMMAND: u16 = 0b11;
 const READ_COMMAND_LENGTH: u8 = 9;
 const STATE_RESPONSE_LENGTH: u8 = 32;
 
 #[main]
-fn main() -> ! {
-    let peripherals = unsafe { Peripherals::steal() };
-    let pin = gpio::Flex::new(peripherals.GPIO16);
-    pin.wait_for_falling_edge();
+fn main(spawner: Spawner) {
+    loop {}
 }
