4b03dfc27b4daea9d07ecf9023e7ed2bb3da33df
Mizu <5565979+l4desu-mizu@users.noreply.github.com>
2025-08-10 21:47:07 +0200

    testing pin switch speed

--- crates/esp32_impl/src/bin/main.rs
+++ crates/esp32_impl/src/bin/main.rs
@@ -6,10 +6,19 @@
     holding buffers for the duration of a data transfer."
 )]
 
-use embassy_executor::Spawner;
-use embassy_time::{Duration, Timer};
+use embassy_executor::{task, Spawner};
+use embassy_time::{Delay, Timer};
+use embedded_hal::delay::DelayNs;
 use esp_hal::clock::CpuClock;
+use esp_hal::gpio::Flex;
+use esp_hal::gpio::Level;
+use esp_hal::gpio::Output;
+use esp_hal::gpio::OutputConfig;
+use esp_hal::gpio::Pull;
+use esp_hal::time::Duration;
 use esp_hal::timer::timg::TimerGroup;
+use esp_hal::timer::PeriodicTimer;
+
 use log::info;
 
 #[panic_handler]
@@ -19,12 +28,29 @@ fn panic(_: &core::panic::PanicInfo) -> ! {
 
 extern crate alloc;
 
+#[task]
+async fn send_task(mut pin: Output<'static>) {
+    let mut delay = Delay;
+    loop {
+        for _ in 0..7 {
+            pin.set_low();
+            delay.delay_us(3);
+            pin.set_high();
+            delay.delay_us(1);
+        }
+        for _ in 0..2 {
+            pin.set_low();
+            delay.delay_us(1);
+            pin.set_high();
+            delay.delay_us(3);
+        }
+        delay.delay_ms(1000);
+    }
+}
 // This creates a default app-descriptor required by the esp-idf bootloader.
 // For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
 esp_bootloader_esp_idf::esp_app_desc!();
-
-#[esp_hal_embassy::main]
-async fn main(spawner: Spawner) {
+async fn run(spawner: Spawner) {
     // generator version: 0.5.0
 
     esp_println::logger::init_logger_from_env();
@@ -34,25 +60,51 @@ async fn main(spawner: Spawner) {
 
     esp_alloc::heap_allocator!(size: 64 * 1024);
 
-    let timer0 = TimerGroup::new(peripherals.TIMG1);
-    esp_hal_embassy::init(timer0.timer0);
+    //let timer0 = TimerGroup::new(peripherals.TIMG1);
+    //esp_hal_embassy::init(timer0.timer0);
+    let timer_group = TimerGroup::new(peripherals.TIMG0);
 
     info!("Embassy initialized!");
 
-    let rng = esp_hal::rng::Rng::new(peripherals.RNG);
-    let timer1 = TimerGroup::new(peripherals.TIMG0);
-    let wifi_init =
-        esp_wifi::init(timer1.timer0, rng).expect("Failed to initialize WIFI/BLE controller");
-    let (mut _wifi_controller, _interfaces) = esp_wifi::wifi::new(&wifi_init, peripherals.WIFI)
-        .expect("Failed to initialize WIFI controller");
-
     // TODO: Spawn some tasks
-    let _ = spawner;
+    let mut my_pin = Output::new(
+        peripherals.GPIO4,
+        Level::High,
+        OutputConfig::default().with_pull(Pull::Up),
+    );
+    //my_pin.set_input_enable(false);
+    my_pin.set_high();
+    /*let s = send_task(my_pin);
 
+    let _ = spawner.spawn(s);*/
+    let mut pin = my_pin;
+    let mut timer = PeriodicTimer::new(timer_group.timer0);
     loop {
-        info!("Hello world!");
-        Timer::after(Duration::from_secs(1)).await;
+        for _ in 0..7 {
+            pin.set_low();
+            timer.start(Duration::from_micros(3));
+            timer.wait();
+            //Timer::after(Duration::from_micros(3)).await;
+            pin.set_high();
+            timer.start(Duration::from_micros(1));
+            timer.wait();
+        }
+        for _ in 0..2 {
+            pin.set_low();
+            timer.start(Duration::from_micros(1));
+            timer.wait();
+            pin.set_high();
+            timer.start(Duration::from_micros(3));
+            timer.wait();
+        }
+        timer.start(Duration::from_secs(1));
+        timer.wait();
+        //Timer::after(Duration::from_secs(1)).await;
     }
+}
 
+#[esp_hal_embassy::main]
+async fn main(spawner: Spawner) {
+    run(spawner).await;
     // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0-rc.0/examples/src/bin
 }
