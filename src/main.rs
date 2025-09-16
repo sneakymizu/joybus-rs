490ab5ef03a0ecc15fef195e606828f980204ba2
Mizu <5565979+l4desu-mizu@users.noreply.github.com>
2025-09-16 22:30:13 +0200

    setup pwm output with specific frequencies

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@
 #![feature(asm_experimental_arch)]
 #![feature(asm_const)]
 
-use arduino_hal::pac::TC1;
 use panic_halt as _;
 use ufmt::uwriteln;
 
@@ -29,21 +28,6 @@ impl<L,R> Either<L,R>{
         }
     }
 }
-struct FrequencyGenerator<const PRESCALER: u16>{
-    pwm_driver: TC1,
-}
-impl<const PRESCALER:u16> FrequencyGenerator<PRESCALER>{
-    const FREQUENCY_CONVERSION_FACTOR:u16=(16000000/(2*PRESCALER as u32)) as u16;
-
-    fn set_freq(&mut self, freq:u16){
-        let top = if freq == 0{
-            0
-        }else{
-            Self::FREQUENCY_CONVERSION_FACTOR*freq
-        };
-        self.pwm_driver.ocr1a.write(|w|w.bits(top));
-    }
-}
 
 #[arduino_hal::entry]
 fn main() -> ! {
@@ -54,10 +38,7 @@ fn main() -> ! {
     timer.tccr0b.write(|w|w.cs0().direct());  // no prescale, normal timer operation
     let pwm_sound_driver = dp.TC1;
     pwm_sound_driver.tccr1a.write(|w|w.com1a().match_toggle().wgm1().bits(1));
-    pwm_sound_driver.tccr1b.write(|w|w.wgm1().bits(2).cs1().prescale_1024());
-    let mut generator = FrequencyGenerator::<1024>{
-        pwm_driver: pwm_sound_driver,
-    };
+    pwm_sound_driver.tccr1b.write(|w|w.wgm1().bits(0b10).cs1().direct());
 
     let pins = arduino_hal::pins!(dp);
     // Digital pin 13 is also connected to an onboard LED marked "L"
@@ -66,12 +47,10 @@ fn main() -> ! {
 
     let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
     let mut _reader_pin = Either::Left(pins.d6.into_output_high().downgrade());
-    generator.set_freq(0);
     pins.d9.into_output();  // oc1a is pb1, which is d9 on arduino nano - setting high for pwm output
 
-    //uwriteln!(serial, "Lets go\r").unwrap();
-    led_pin.set_low();
     arduino_hal::delay_ms(3000);
+    led_pin.set_low();
     _reader_pin = Either::Right(_reader_pin.left().into_pull_up_input());
     const DATA_LEN:usize=4;
     let mut data = [0u8;DATA_LEN];
@@ -88,41 +67,30 @@ fn main() -> ! {
             },
         };
         let state: N64ControllerState = data.into();
-        let _ = uwriteln!(serial, "A is {}\r", if state.a_button(){"pressed"}else{"released"});
-        let _ = uwriteln!(serial, "B is {}\r", if state.b_button(){"pressed"}else{"released"});
-        let _ = uwriteln!(serial, "Z is {}\r", if state.z_button(){"pressed"}else{"released"});
-        let _ = uwriteln!(serial, "C up is {}\r", if state.c_up(){"pressed"}else{"released"});
-        let _ = uwriteln!(serial, "C down {}\r", if state.c_down(){"pressed"}else{"released"});
-        let _ = uwriteln!(serial, "C left {}\r", if state.c_left(){"pressed"}else{"released"});
-        let _ = uwriteln!(serial, "C right {}\r", if state.c_right(){"pressed"}else{"released"});
-        let _ = uwriteln!(serial, "Reset is {}\r", if state.reset(){"pressed"}else{"released"});
-        let _ = uwriteln!(serial, "Start is {}\r", if state.start_button(){"pressed"}else{"released"});
-        let _ = uwriteln!(serial, "Right trigger is {}\r", if state.right_trigger(){"pressed"}else{"released"});
-        let _ = uwriteln!(serial, "Left trigger {}\r", if state.left_trigger(){"pressed"}else{"released"});
-        let _ = uwriteln!(serial, "Dpad up is {}\r", if state.dpad_up(){"pressed"}else{"released"});
-        let _ = uwriteln!(serial, "Dpad down is {}\r", if state.dpad_down(){"pressed"}else{"released"});
-        let _ = uwriteln!(serial, "Dpad left is {}\r", if state.dpad_left(){"pressed"}else{"released"});
-        let _ = uwriteln!(serial, "Dpad right is {}\r", if state.dpad_right(){"pressed"}else{"released"});
-        let _ = uwriteln!(serial, "X is {}\r", state.x_axis());
-        let _ = uwriteln!(serial, "Y is {}\r", state.y_axis());
 
-        if state.a_button(){
-            generator.set_freq(294);
+        let top = if state.a_button(){
+            frequency_into_top(294)
         }
         else if state.c_down(){
-            generator.set_freq(349);
+            frequency_into_top(349)
         }
         else if state.c_right(){
-            generator.set_freq(440);
+            frequency_into_top(440)
         }
         else if state.c_left(){
-            generator.set_freq(494);
+            frequency_into_top(494)
         }
         else if state.c_up(){
-            generator.set_freq(587);
+            frequency_into_top(587)
         }else{
-            generator.set_freq(0);
-        }
-        arduino_hal::delay_ms(500);
+            0
+        };
+        pwm_sound_driver.ocr1a.write(|w|w.bits(top));
     }
+}
+
+fn frequency_into_top(freq: u16)->u16{
+    // only works for 16 bit phase and frequency correct timer
+    let res = 16000000/(4*freq as u32);
+    res as u16
 }
