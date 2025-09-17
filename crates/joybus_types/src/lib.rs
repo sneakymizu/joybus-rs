fe74472e89358f933d52647875b5ebb4f86c9479
Mizu <5565979+l4desu-mizu@users.noreply.github.com>
2025-09-17 23:08:19 +0200

    run formatter

--- crates/joybus_types/src/lib.rs
+++ crates/joybus_types/src/lib.rs
@@ -1,70 +1,70 @@
 #![no_std]
 
-pub struct N64ControllerState([u8;4]);
-impl From<[u8;4]> for N64ControllerState{
-    fn from(value: [u8;4]) -> Self {
+pub struct N64ControllerState([u8; 4]);
+impl From<[u8; 4]> for N64ControllerState {
+    fn from(value: [u8; 4]) -> Self {
         N64ControllerState(value)
     }
 }
-impl From<u32> for N64ControllerState{
-    fn from(value: u32)->Self{
+impl From<u32> for N64ControllerState {
+    fn from(value: u32) -> Self {
         N64ControllerState(value.to_le_bytes())
     }
 }
-impl N64ControllerState{
-    pub fn a_button(&self)->bool{
-        (self.0[0]&128u8) != 0
+impl N64ControllerState {
+    pub fn a_button(&self) -> bool {
+        (self.0[0] & 128u8) != 0
     }
-    pub fn b_button(&self)->bool{
-        (self.0[0]&64u8) != 0
+    pub fn b_button(&self) -> bool {
+        (self.0[0] & 64u8) != 0
     }
-    pub fn z_button(&self)->bool{
-        (self.0[0]&32u8) != 0
+    pub fn z_button(&self) -> bool {
+        (self.0[0] & 32u8) != 0
     }
-    pub fn start_button(&self)->bool{
-        (self.0[0]&16u8) != 0
+    pub fn start_button(&self) -> bool {
+        (self.0[0] & 16u8) != 0
     }
-    pub fn dpad_up(&self)->bool{
-        (self.0[0]&8u8) != 0
+    pub fn dpad_up(&self) -> bool {
+        (self.0[0] & 8u8) != 0
     }
-    pub fn dpad_down(&self)->bool{
-        (self.0[0]&4u8) != 0
+    pub fn dpad_down(&self) -> bool {
+        (self.0[0] & 4u8) != 0
     }
-    pub fn dpad_left(&self)->bool{
-        (self.0[0]&2u8) != 0
+    pub fn dpad_left(&self) -> bool {
+        (self.0[0] & 2u8) != 0
     }
-    pub fn dpad_right(&self)->bool{
-        (self.0[0]&1u8) != 0
+    pub fn dpad_right(&self) -> bool {
+        (self.0[0] & 1u8) != 0
     }
-    pub fn reset(&self)->bool{
-        (self.0[1]&128u8)!=0
+    pub fn reset(&self) -> bool {
+        (self.0[1] & 128u8) != 0
     }
-    pub fn left_trigger(&self)->bool{
-        (self.0[1]&32u8)!=0
+    pub fn left_trigger(&self) -> bool {
+        (self.0[1] & 32u8) != 0
     }
-    pub fn right_trigger(&self)->bool{
-        (self.0[1]&16u8)!=0
+    pub fn right_trigger(&self) -> bool {
+        (self.0[1] & 16u8) != 0
     }
-    pub fn c_up(&self)->bool{
-        (self.0[1]&8u8)!=0
+    pub fn c_up(&self) -> bool {
+        (self.0[1] & 8u8) != 0
     }
-    pub fn c_down(&self)->bool{
-        (self.0[1]&4u8)!=0
+    pub fn c_down(&self) -> bool {
+        (self.0[1] & 4u8) != 0
     }
-    pub fn c_left(&self)->bool{
-        (self.0[1]&2u8)!=0
+    pub fn c_left(&self) -> bool {
+        (self.0[1] & 2u8) != 0
     }
-    pub fn c_right(&self)->bool{
-        (self.0[1]&1u8)!=0
+    pub fn c_right(&self) -> bool {
+        (self.0[1] & 1u8) != 0
     }
-    pub fn x_axis(&self)->i8{
+    pub fn x_axis(&self) -> i8 {
         self.0[2] as i8
     }
-    pub fn y_axis(&self)->i8{
+    pub fn y_axis(&self) -> i8 {
         self.0[3] as i8
     }
 }
 
-pub mod commands{
-    pub const POLL_SIGNAL:u8 = 0b00000001u8;
+pub mod commands {
+    pub const POLL_SIGNAL: u8 = 0b00000001u8;
 }
--- src/main.rs
+++ src/main.rs
@@ -10,21 +10,21 @@ use joybus_atmega328p::{read_bytes, send_byte, ReadError};
 
 use joybus_types::N64ControllerState;
 
-enum Either<L,R>{
+enum Either<L, R> {
     Left(L),
-    Right(R)
+    Right(R),
 }
-impl<L,R> Either<L,R>{
-    fn left(self)->L{
-        match self{
-            Either::Left(l)=>l,
-            Either::Right(_r)=>panic!()
+impl<L, R> Either<L, R> {
+    fn left(self) -> L {
+        match self {
+            Either::Left(l) => l,
+            Either::Right(_r) => panic!(),
         }
     }
-    fn right(self)->R{
-        match self{
-            Either::Left(_l)=>panic!(),
-            Either::Right(r)=>r
+    fn right(self) -> R {
+        match self {
+            Either::Left(_l) => panic!(),
+            Either::Right(r) => r,
         }
     }
 }
@@ -35,10 +35,14 @@ fn main() -> ! {
     let timer = dp.TC0;
     // normal operating timer
     timer.tccr0a.reset();
-    timer.tccr0b.write(|w|w.cs0().direct());  // no prescale, normal timer operation
+    timer.tccr0b.write(|w| w.cs0().direct()); // no prescale, normal timer operation
     let pwm_sound_driver = dp.TC1;
-    pwm_sound_driver.tccr1a.write(|w|w.com1a().match_toggle().wgm1().bits(1));
-    pwm_sound_driver.tccr1b.write(|w|w.wgm1().bits(0b10).cs1().direct());
+    pwm_sound_driver
+        .tccr1a
+        .write(|w| w.com1a().match_toggle().wgm1().bits(1));
+    pwm_sound_driver
+        .tccr1b
+        .write(|w| w.wgm1().bits(0b10).cs1().direct());
 
     let pins = arduino_hal::pins!(dp);
     // Digital pin 13 is also connected to an onboard LED marked "L"
@@ -47,67 +51,77 @@ fn main() -> ! {
 
     let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
     let mut _reader_pin = Either::Left(pins.d6.into_output_high().downgrade());
-    pins.d9.into_output();  // oc1a is pb1, which is d9 on arduino nano - setting high for pwm output
+    pins.d9.into_output(); // oc1a is pb1, which is d9 on arduino nano - setting high for pwm output
 
     arduino_hal::delay_ms(3000);
     led_pin.set_low();
     _reader_pin = Either::Right(_reader_pin.left().into_pull_up_input());
-    const DATA_LEN:usize=4;
-    let mut data = [0u8;DATA_LEN];
+    const DATA_LEN: usize = 4;
+    let mut data = [0u8; DATA_LEN];
     loop {
         _reader_pin = Either::Left(_reader_pin.right().into_output_high());
-        unsafe {send_byte::<0x0b, 0x06, 1>([joybus_types::commands::POLL_SIGNAL])};
+        unsafe { send_byte::<0x0b, 0x06, 1>([joybus_types::commands::POLL_SIGNAL]) };
         _reader_pin = Either::Right(_reader_pin.left().into_pull_up_input());
-        let _ = match unsafe{read_bytes::<0x9, 0x6, 0x26, 0x15, 1, DATA_LEN>(&mut data)}{
+        let _ = match unsafe { read_bytes::<0x9, 0x6, 0x26, 0x15, 1, DATA_LEN>(&mut data) } {
             Ok(b) => uwriteln!(serial, "(Stop-bit) Bytes are {:?} {:?}\r", b, data),
-            Err(ReadError::OutOfMemory(len)) => uwriteln!(serial, "(No Stopbit) Bytes are {:?}: {:?}\r", len, data),
+            Err(ReadError::OutOfMemory(len)) => {
+                uwriteln!(serial, "(No Stopbit) Bytes are {:?}: {:?}\r", len, data)
+            }
             Err(e) => {
                 let _ = uwriteln!(serial, "Got error {:?}\r", e);
                 continue;
-            },
+            }
         };
 
         let state: N64ControllerState = data.into();
-        let power: Option<i8> = if state.c_right(){ // A
+        let power: Option<i8> = if state.c_right() {
+            // A
             Some(0)
-        } else if state.c_down(){ // F
+        } else if state.c_down() {
+            // F
             Some(-4)
-        } else if state.a_button(){ // D
+        } else if state.a_button() {
+            // D
             Some(-7)
-        } else if state.c_left(){ // B
+        } else if state.c_left() {
+            // B
             Some(2)
-        } else if state.c_up(){ // D
+        } else if state.c_up() {
+            // D
             Some(5)
-        }else{
+        } else {
             None
         };
-        if power.is_none(){
-            pwm_sound_driver.ocr1a.write(|w|w.bits(0));
+        if power.is_none() {
+            pwm_sound_driver.ocr1a.write(|w| w.bits(0));
             continue;
         }
-        let power = power.unwrap() as i8 - state.z_button() as i8 + state.y_axis().signum() * 2 + state.right_trigger() as i8;
+        let power = power.unwrap() as i8
+            - state.z_button() as i8  // augments half step down
+            + state.y_axis().signum() * 2 // augments a whole step
+            + state.right_trigger() as i8; // augments half step up
         let freq = calculate_equal_temperate_frequency::<440>(power);
         let top = frequency_into_top(freq);
-        pwm_sound_driver.ocr1a.write(|w|w.bits(top));
+        pwm_sound_driver.ocr1a.write(|w| w.bits(top));
     }
 }
 
-const SEMITONE_FACTOR:f32 = 1.05946309436;
-fn calculate_equal_temperate_frequency<const BASE_FREQUENCY:u32>(power: i8)->u16{
-    let mut frequency=BASE_FREQUENCY as f32;
-    let fun = if power>=0{
+const SEMITONE_FACTOR: f32 = 1.05946309436;
+fn calculate_equal_temperate_frequency<const BASE_FREQUENCY: u32>(power: i8) -> u16 {
+    let mut frequency = BASE_FREQUENCY as f32;
+    let fun = if power >= 0 {
         <f32 as core::ops::Mul>::mul
-    }else{
+    } else {
         <f32 as core::ops::Div>::div
     };
-    for _ in 0..power.abs(){
+    for _ in 0..power.abs() {
         frequency = fun(frequency, SEMITONE_FACTOR);
     }
     (frequency + 0.5) as u16
 }
 
-fn frequency_into_top(freq: u16)->u16{
+fn frequency_into_top(freq: u16) -> u16 {
     // only works for 16 bit phase and frequency correct timer
-    let res = 16000000/(4*freq as u32);
+    let res = 16000000 / (4 * freq as u32);
     res as u16
 }
