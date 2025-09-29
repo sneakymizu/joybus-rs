99c1f561fdce44e53ff63d166e5948a784cbca7b
Mizu <5565979+l4desu-mizu@users.noreply.github.com>
2025-09-29 21:01:24 +0200

    aligning atmega implementation naming scheme

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use arduino_hal::clock::Clock;
 use panic_halt as _;
 use ufmt::{derive::uDebug, uwriteln};
 
-use joybus_atmega328p::{read_bytes, send_byte, ReadError};
+use joybus_rs_atmega328p::{read_bytes, send_byte, ReadError};
 
 use joybus_rs::JoybusControllerState;
 
