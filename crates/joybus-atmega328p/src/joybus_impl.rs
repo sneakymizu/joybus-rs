9aed1489264235114c8167ac022ea57ab59d6f7f
Mizu <5565979+l4desu-mizu@users.noreply.github.com>
2025-09-30 09:06:21 +0200

    try to establish a glue layer for atmega joybus implementation

--- crates/joybus-atmega328p/src/joybus_impl.rs
+++ crates/joybus-atmega328p/src/joybus_impl.rs
@@ -1,57 +1,49 @@
+use core::marker::PhantomData;
+
 use embedded_hal::digital::{InputPin, OutputPin};
 use joybus_rs::JoybusConsole;
 
 use crate::{read_bytes, send_byte};
 
-enum SwappablePin<L, R> {
-    Left(L),
-    Right(R),
+pub struct OutputPinData{
+    pub pin_number: u8,
+    pub port: u8,
 }
-impl<L, R> SwappablePin<L, R> {
-    fn left(&mut self) -> L {
-        match self {
-            SwappablePin::Left(l) => l,
-            SwappablePin::Right(_r) => panic!("Right was unassigned but you tried to access it."),
-        }
-    }
-    fn right(&mut self) -> R {
-        match self {
-            SwappablePin::Left(_l) => panic!("Left was unassigned but you tried to access it."),
-            SwappablePin::Right(r) => r,
-        }
-    }
+pub struct InputPinData{
+    pub pin_number: u8,
+    pub pin:u8
 }
-trait SwappablePinQuestionMark {
-    fn into_output();
-    fn into_input();
-    fn get_port();
-    fn get_pin();
+pub trait SwappablePin {
+    fn as_output(&mut self)->&OutputPinData;
+    fn as_input(&mut self)->&InputPinData;
 }
 
-struct JoybusPin<I: InputPin, O: OutputPin> {
-    pin: SwappablePin<I, O>,
+pub struct JoybusPin<P: SwappablePin> {
+    pin: P,
 }
 
-impl<I: InputPin, O: OutputPin> JoybusPin<I, O> {
-    fn from_input(input: I) -> Self {
+impl<P: SwappablePin> JoybusPin<P> {
+    pub fn from_pin(pin: P) -> Self {
         Self {
-            pin: SwappablePin::Left(input),
-        }
-    }
-    fn from_output(output: O) -> Self {
-        Self {
-            pin: SwappablePin::Right(output),
+            pin,
         }
     }
 }
 
-impl<I: InputPin, O: OutputPin> JoybusConsole for JoybusPin<I, O> {
+impl<P: SwappablePin> JoybusConsole for JoybusPin<P> {
     fn read<const COMMAND: u8>(
         &mut self,
         data: &mut [u8],
     ) -> Result<usize, joybus_rs::JoybusError> {
+        match self.pin.as_output(){
+            OutputPinData { pin_number, port }=>unsafe {
+                send_byte::<0x0b, 0x06, 1>([joybus_rs::commands::POLL_SIGNAL])
+            }
+        }
         unsafe { send_byte::<0x0b, 0x06, 1>([joybus_rs::commands::POLL_SIGNAL]) };
+        self.pin.as_input();
         let _ = unsafe { read_bytes::<0x9, 0x6, 0x26, 0x15, 1, 4>(data) };
+        self.pin.as_output();
         Ok(0)
     }
 
