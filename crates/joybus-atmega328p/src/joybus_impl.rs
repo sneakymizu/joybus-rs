45c20e71baf6bfaa29fa3de8ed31170ad6def4c2
Mizu <5565979+l4desu-mizu@users.noreply.github.com>
2025-10-06 00:12:16 +0200

    define reading trait to implement for all pins, abstracting implementation selection

--- crates/joybus-atmega328p/src/joybus_impl.rs
+++ crates/joybus-atmega328p/src/joybus_impl.rs
@@ -3,13 +3,13 @@ use core::any::{Any, TypeId};
 use arduino_hal::{
     hal::port::{self, Dynamic, PB1},
     port::{
-        mode::{Io, Output},
+        mode::{Floating, Input, Io, Output},
         Pin, PinMode, PinOps,
     },
 };
 use joybus_rs::JoybusConsole;
 
-use crate::{read_bytes, send_byte};
+use crate::{read_bytes, send_byte, ReadError};
 
 pub struct JoybusPin<PIN> {
     pin: PIN,
@@ -21,17 +21,86 @@ impl<PIN> JoybusPin<PIN> {
     }
 }
 
-trait Read<const R: usize, const W: usize> {
-    fn read(&mut self, send: &[u8; W], recv: &mut [u8; R]);
+struct PinWrapper<PIN: PinOps>{
+    input: Option<Pin<Input<Floating>, PIN>>,
+    output: Option<Pin<Output, PIN>>
 }
-impl<const R: usize, const W: usize> Read<R, W> for Pin<Dynamic, port::PB0> {
-    fn read(&mut self, send: &[u8; W], recv: &mut [u8; R]) {
-        unsafe {
-            self.make_output();
-        };
+impl<PIN: PinOps> PinWrapper<PIN>{
+    pub fn from_pin(pin:Pin<Input<Floating>, PIN>)->Self{
+        Self { input: Some(pin), output: None }
+    }
+}
+
+trait JoybusPinRead<const R: usize, const W: usize> {
+    fn joybus_read(&mut self, send: &[u8; W], recv: &mut [u8; R])->Result<usize,ReadError>;
+}
+
+impl<const R: usize, const W: usize> JoybusPinRead<R, W> for PinWrapper<port::PB0> {
+    fn joybus_read(&mut self, send: &[u8; W], recv: &mut [u8; R]) -> Result<usize, ReadError>{
+        let output = self.input.take().unwrap().into_output_high();
+        unsafe { send_byte::<0x0b, 0x00, W>(send) };
+        self.input = Some(output.into_floating_input());
+        unsafe { read_bytes::<0x9, 0x0, 0x26, 0x15, 1, R>(recv) }.map(|v| v as usize)
+    }
+}
+
+impl<const R: usize, const W: usize> JoybusPinRead<R, W> for PinWrapper<port::PB1> {
+    fn joybus_read(&mut self, send: &[u8; W], recv: &mut [u8; R]) -> Result<usize, ReadError>{
+        let output = self.input.take().unwrap().into_output_high();
+        unsafe { send_byte::<0x0b, 0x01, W>(send) };
+        self.input = Some(output.into_floating_input());
+        unsafe { read_bytes::<0x9, 0x1, 0x26, 0x15, 1, R>(recv) }.map(|v| v as usize)
+    }
+}
+
+impl<const R: usize, const W: usize> JoybusPinRead<R, W> for PinWrapper<port::PB2> {
+    fn joybus_read(&mut self, send: &[u8; W], recv: &mut [u8; R]) -> Result<usize, ReadError>{
+        let output = self.input.take().unwrap().into_output_high();
+        unsafe { send_byte::<0x0b, 0x02, W>(send) };
+        self.input = Some(output.into_floating_input());
+        unsafe { read_bytes::<0x9, 0x2, 0x26, 0x15, 1, R>(recv) }.map(|v| v as usize)
+    }
+}
+
+impl<const R: usize, const W: usize> JoybusPinRead<R, W> for PinWrapper<port::PB3> {
+    fn joybus_read(&mut self, send: &[u8; W], recv: &mut [u8; R]) -> Result<usize, ReadError>{
+        let output = self.input.take().unwrap().into_output_high();
+        unsafe { send_byte::<0x0b, 0x03, W>(send) };
+        self.input = Some(output.into_floating_input());
+        unsafe { read_bytes::<0x9, 0x3, 0x26, 0x15, 1, R>(recv) }.map(|v| v as usize)
+    }
+}
+
+impl<const R: usize, const W: usize> JoybusPinRead<R, W> for PinWrapper<port::PB4> {
+    fn joybus_read(&mut self, send: &[u8; W], recv: &mut [u8; R]) -> Result<usize, ReadError>{
+        let output = self.input.take().unwrap().into_output_high();
+        unsafe { send_byte::<0x0b, 0x04, W>(send) };
+        self.input = Some(output.into_floating_input());
+        unsafe { read_bytes::<0x9, 0x4, 0x26, 0x15, 1, R>(recv) }.map(|v| v as usize)
+    }
+}
+impl<const R: usize, const W: usize> JoybusPinRead<R, W> for PinWrapper<port::PB5> {
+    fn joybus_read(&mut self, send: &[u8; W], recv: &mut [u8; R]) -> Result<usize, ReadError>{
+        let output = self.input.take().unwrap().into_output_high();
+        unsafe { send_byte::<0x0b, 0x05, W>(send) };
+        self.input = Some(output.into_floating_input());
+        unsafe { read_bytes::<0x9, 0x5, 0x26, 0x15, 1, R>(recv) }.map(|v| v as usize)
+    }
+}
+impl<const R: usize, const W: usize> JoybusPinRead<R, W> for PinWrapper<port::PB6> {
+    fn joybus_read(&mut self, send: &[u8; W], recv: &mut [u8; R]) -> Result<usize, ReadError>{
+        let output = self.input.take().unwrap().into_output_high();
         unsafe { send_byte::<0x0b, 0x06, W>(send) };
-        self.into_input();
-        let _ = unsafe { read_bytes::<0x9, 0x6, 0x26, 0x15, 1, R>(recv) };
+        self.input = Some(output.into_floating_input());
+        unsafe { read_bytes::<0x9, 0x6, 0x26, 0x15, 1, R>(recv) }.map(|v| v as usize)
+    }
+}
+impl<const R: usize, const W: usize> JoybusPinRead<R, W> for PinWrapper<port::PB7> {
+    fn joybus_read(&mut self, send: &[u8; W], recv: &mut [u8; R]) -> Result<usize, ReadError>{
+        let output = self.input.take().unwrap().into_output_high();
+        unsafe { send_byte::<0x0b, 0x07, W>(send) };
+        self.input = Some(output.into_floating_input());
+        unsafe { read_bytes::<0x9, 0x7, 0x26, 0x15, 1, R>(recv) }.map(|v| v as usize)
     }
 }
 
